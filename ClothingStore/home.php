659015babd4f1968ced6f4e0231d25d332a38faf
LuksTheGreat <167790963+LuksTheGreat@users.noreply.github.com>
2026-05-04 17:11:43 +0200

    Add files via upload
    done just needs the video still

--- ClothingStore/home.php
+++ ClothingStore/home.php
@@ -7,8 +7,12 @@ if(!isset($_SESSION['user'])){
 }
 ?>
 
+<link rel="stylesheet" href="style.css">
+
+<div class="container">
 <h1>Welcome <?php echo $_SESSION['user']; ?></h1>
 
-<a href="addProduct.php">Add Product</a><br>
-<a href="viewProducts.php">View Store</a><br>
+<a href="addProduct.php">Add Product</a><br><br>
+<a href="viewProducts.php">View Store</a><br><br>
 <a href="logout.php">Logout</a>
+</div>
--- ClothingStore/login.php
+++ ClothingStore/login.php
@@ -2,24 +2,39 @@
 session_start();
 include 'DBConn.php';
 
+$message = "";
+
 if(isset($_POST['login'])){
     $username = $_POST['username'];
     $password = $_POST['password'];
 
     $result = $conn->query("SELECT * FROM tblUser WHERE username='$username'");
-    $user = $result->fetch_assoc();
 
-    if($user && password_verify($password, $user['password'])){
-        $_SESSION['user'] = $user['username'];
-        header("Location: home.php");
+    if($result->num_rows > 0){
+        $row = $result->fetch_assoc();
+
+        if(password_verify($password, $row['password'])){
+            $_SESSION['user'] = $username;
+            header("Location: home.php");
+            exit();
+        } else {
+            $message = "Wrong password!";
+        }
     } else {
-        echo "Invalid login";
+        $message = "User not found!";
     }
 }
 ?>
 
+<link rel="stylesheet" href="style.css">
+
+<div class="container">
+<h2>Login</h2>
 <form method="POST">
-<input type="text" name="username"><br>
-<input type="password" name="password"><br>
+<input type="text" name="username" placeholder="Username" required>
+<input type="password" name="password" placeholder="Password" required>
 <button name="login">Login</button>
 </form>
+
+<p><?php echo $message; ?></p>
+</div>
